37aa0123c595321d31e40256fa88b9e9aa6e2376
Leonardo Reis <leoeareis@gmail.com>
2024-05-14 21:00:42 +0200

    Add coverage threshold

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,14 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    coverage: {
+      thresholds: {
+        lines: 80,
+        functions: 80,
+        branches: 80,
+        statements: 80,
+      },
+    },
+  },
 });
