6c00d00a040f0dc83749548c9a92ad1609a0129a
Lucas W. da Motta <lucaswantz@hotmail.com>
2024-05-14 15:12:21 -0300

    Merge

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,8 @@
-import express from "express";
 import cors from "cors";
+import express from "express";
 
-import { clienteRouter } from "./Infrastructure/api/routes/ClienteRouter";
+import { pagamentoRouter } from "./Infrastructure/api/routes/PagamentoRouter";
 import { produtoRouter } from "./Infrastructure/api/routes/ProdutoRouter";
-import { pedidoRouter } from "./Infrastructure/api/routes/PedidoRouter";
 
 import { errorMiddleware } from "./Infrastructure/api/middlewares/error";
 
@@ -12,9 +11,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/cliente", clienteRouter);
-app.use("/produto", produtoRouter);
-app.use("/pedido", pedidoRouter);
+app.use("/pagamento", pagamentoRouter);
+app.use("/webhook", produtoRouter);
 
 app.use(errorMiddleware);
 
