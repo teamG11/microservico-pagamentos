39ca811ea777f032e9474a8abb77934a25578441
Leonardo Reis <leoeareis@gmail.com>
2024-05-19 18:26:02 +0200

    Altera config testes

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,15 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    coverage: {
+      exclude: [
+        "**/*.spec.ts", // Exclui especificamente arquivos .spec.ts
+        "**/tests/**", // Exclui qualquer coisa dentro de uma pasta tests
+        "**/__tests__/**", // Exclui qualquer coisa dentro de uma pasta __tests__
+        "src/Infrastructure/lib/prisma.ts",
+        "src/Infrastructure/env/index.ts",
+      ],
+    },
+  },
 });
